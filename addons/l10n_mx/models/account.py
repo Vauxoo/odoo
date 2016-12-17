M4GiK/kamserp-v10

[IMP] l10n_mx: Completely refactored Mexican CoA Template. Courtesy of Vauxoo.
* Added accounts provided following the structure provided by the SAT
this accounts are only the minimal necessary ones, with the objective
to set the first easiest engagement to new users and tags will represent the actual information
from the document linked.

* The name of the tag is the concatenation of tag.code + tag.name, because the account.tag
model have not the code field.

Note: we will need more accounts but some of them for special operations
which are not basic at all, then it is better just propose the operational accounts
once we understand a proper closing process.

* Set the proper user-types on accounts in order to be able to use it with the official reports this CoA
for operational purpose.

* Added nature field and data in tags to allow set this value in electronic account report, and
set this value with data pre loaded, we did not use user.type for this due to the fact that the same user type can have different nature (if it grows for credit or debit), I do not know if this field brakes the stable rule we can discuse about that.

* Assign the account tags with in the Mexican CoA template installation, and with an onchange(account.code) when manually modifying/creating accounts.

Rationale: If the code of the accounts follows the pattern 111.00.00 or 111-00-00 then it will
set the tag 111.00 if the tag exists, if not then this will be ignored.

* Generate an account tag by each second level account in SAT catalog, and assign to the corresponding account.

addons/l10n_mx/models/account.py
@@ -1,0 +1,59 @@
+# coding: utf-8
+# Copyright 2016 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+import re
+from odoo import models, api, fields, _
+
+
+class AccountJournal(models.Model):
+    _inherit = 'account.journal'
+
+    @api.model
+    def _prepare_liquidity_account(self, name, company, currency_id, type):
+        '''
+        When preparing the values to use when creating the default debit and credit accounts of a
+        liquidity journal, set the correct tags for the mexican localization.
+        '''
+        res = super(AccountJournal, self)._prepare_liquidity_account(name, company, currency_id, type)
+        if company.country_id.id == self.env.ref('base.mx').id:
+            mx_tags = self.env['account.account'].mx_search_tags(res.get('code', ''))
+            if mx_tags:
+                res.update({
+                    'tag_ids': [(6, 0, [tag.id for tag in mx_tags])]
+                })
+        return res
+
+class AccountAccount(models.Model):
+    _inherit = 'account.account'
+
+    @api.model
+    def mx_search_tags(self, code):
+        account_tag = self.env['account.account.tag']
+        #search if the code is compliant with the regexp we have for tags auto-assignation
+        re_res = re.search(
+            '^(?P<first>[1-8][0-9][0-9])[,.]'
+            '(?P<second>[0-9][0-9])[,.]'
+            '(?P<third>[0-9]{2,3})$', code)
+        if not re_res:
+            return account_tag
+
+        #get the elements of that code divided with separation declared in the regexp
+        account = re_res.groups()
+        return account_tag.search([
+            ('name', '=like', "%s.%s%%" % (account[0], account[1])),
+            ('color', '=', 4)], limit=1)
+
+    @api.onchange('code')
+    def _onchange_code(self):
+        if self.company_id.country_id.id == self.env.ref('base.mx').id and self.code:
+            tags = self.mx_search_tags(self.code)
+            self.tag_ids = tags
+
+
+class AccountAccountTag(models.Model):
+    _inherit = 'account.account.tag'
+
+    nature = fields.Selection([
+        ('D', _('Debitable Account')), ('A', _('Creditable Account'))],
+        help='Used in Mexican report of electronic accounting (account nature).')

addons/l10n_mx/models/chart_template.py
@@ -1,0 +1,81 @@
+# coding: utf-8
+# Copyright 2016 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models, api, _
+
+
+class AccountChartTemplate(models.Model):
+    _inherit = "account.chart.template"
+
+    @api.multi
+    def _load_template(
+            self, company, code_digits=None, transfer_account_id=None,
+            account_ref=None, taxes_ref=None):
+        """
+        Set the 'use_cash_basis' and 'cash_basis_account' fields on account.account. This hack is needed due to the fact
+        that the tax template does not have the fields 'use_cash_basis' and 'cash_basis_account'.
+
+        This hunk should be removed in master, as the account_tax_cash_basis module has been merged already in account
+        module
+        """
+        self.ensure_one()
+        accounts, taxes = super(AccountChartTemplate, self)._load_template(
+            company, code_digits=code_digits,
+            transfer_account_id=transfer_account_id, account_ref=account_ref,
+            taxes_ref=taxes_ref)
+        if not self == self.env.ref('l10n_mx.mx_coa'):
+            return accounts, taxes
+        account_tax_obj = self.env['account.tax']
+        account_obj = self.env['account.account']
+        taxes_acc = {
+            'ITAX_010-IN': account_obj.search([('code', '=', '208.01.01')]),
+            'ITAX_160-IN': account_obj.search([('code', '=', '208.01.01')]),
+            'ITAXR_04-OUT': account_obj.search([('code', '=', '216.13.01')]),
+            'ITAXR_10-OUT': account_obj.search([('code', '=', '216.13.01')]),
+            'ITAX_1067-OUT': account_obj.search([('code', '=', '216.13.01')]),
+            'ITAX_167-OUT': account_obj.search([('code', '=', '216.13.01')]),
+            'ITAX_010-OUT': account_obj.search([('code', '=', '208.01.01')]),
+            'ITAX_160-OUT': account_obj.search([('code', '=', '208.01.01')])}
+
+        for tax in self.tax_template_ids:
+            if tax.description not in taxes_acc:
+                continue
+            account_tax_obj.browse(taxes.get(tax.id)).write({
+                'use_cash_basis': True,
+                'cash_basis_account': taxes_acc.get(tax.description).id,
+            })
+        return accounts, taxes
+
+    @api.model
+    def generate_journals(self, acc_template_ref, company, journals_dict=None):
+        """Set the tax_cash_basis_journal_id on the company"""
+        res = super(AccountChartTemplate, self).generate_journals(
+            acc_template_ref, company, journals_dict=journals_dict)
+        if not self == self.env.ref('l10n_mx.mx_coa'):
+            return res
+        journal_basis = self.env['account.journal'].search([
+            ('type', '=', 'general'),
+            ('code', '=', 'CBMX')], limit=1)
+        company.write({'tax_cash_basis_journal_id': journal_basis.id})
+        return res
+
+    @api.multi
+    def _prepare_all_journals(self, acc_template_ref, company, journals_dict=None):
+        """Create the tax_cash_basis_journal_id"""
+        res = super(AccountChartTemplate, self)._prepare_all_journals(
+            acc_template_ref, company, journals_dict=journals_dict)
+        if not self == self.env.ref('l10n_mx.mx_coa'):
+            return res
+        res.append({
+            'type': 'general',
+            'name': _('Effectively Paid'),
+            'code': 'CBMX',
+            'company_id': company.id,
+            'default_credit_account_id': acc_template_ref.get(
+                self.income_currency_exchange_account_id.id),
+            'default_debit_account_id': acc_template_ref.get(
+                self.expense_currency_exchange_account_id.id),
+            'show_on_dashboard': True,
+        })
+        return res

Vauxoo/odoo

addons/l10n_mx/models/account.py
@@ -1,0 +1,59 @@
+# coding: utf-8
+# Copyright 2016 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+import re
+from openerp import models, api, fields, _
+
+
+class AccountJournal(models.Model):
+    _inherit = 'account.journal'
+
+    @api.model
+    def _prepare_liquidity_account(self, name, company, currency_id, type):
+        '''
+        When preparing the values to use when creating the default debit and credit accounts of a
+        liquidity journal, set the correct tags for the mexican localization.
+        '''
+        res = super(AccountJournal, self)._prepare_liquidity_account(name, company, currency_id, type)
+        if company.country_id.id == self.env.ref('base.mx').id:
+            mx_tags = self.env['account.account'].mx_search_tags(res.get('code', ''))
+            if mx_tags:
+                res.update({
+                    'tag_ids': [(6, 0, [tag.id for tag in mx_tags])]
+                })
+        return res
+
+class AccountAccount(models.Model):
+    _inherit = 'account.account'
+
+    @api.model
+    def mx_search_tags(self, code):
+        account_tag = self.env['account.account.tag']
+        #search if the code is compliant with the regexp we have for tags auto-assignation
+        re_res = re.search(
+            '^(?P<first>[1-8][0-9][0-9])[,.]'
+            '(?P<second>[0-9][0-9])[,.]'
+            '(?P<third>[0-9]{2,3})$', code)
+        if not re_res:
+            return account_tag
+
+        #get the elements of that code divided with separation declared in the regexp
+        account = re_res.groups()
+        return account_tag.search([
+            ('name', '=like', "%s.%s%%" % (account[0], account[1])),
+            ('color', '=', 4)], limit=1)
+
+    @api.onchange('code')
+    def _onchange_code(self):
+        if self.company_id.country_id.id == self.env.ref('base.mx').id and self.code:
+            tags = self.mx_search_tags(self.code)
+            self.tag_ids = tags
+
+
+class AccountAccountTag(models.Model):
+    _inherit = 'account.account.tag'
+
+    nature = fields.Selection([
+        ('D', _('Debitable Account')), ('A', _('Creditable Account'))],
+        help='Used in Mexican report of electronic accounting (account nature).')

addons/l10n_mx/models/chart_template.py
@@ -1,50 +1,7 @@
 # coding: utf-8
 # Copyright 2016 Vauxoo (https://www.vauxoo.com) <info@vauxoo.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-from openerp import models, fields, api
-
-
-class AccountTaxTemplate(models.Model):
-    _inherit = 'account.tax.template'
-
-    use_cash_basis = fields.Boolean(
-        help='Select this if the tax should use cash basis, which will '
-        'create an entry for this tax on a given account during '
-        'reconciliation')
-    cash_basis_account = fields.Many2one(
-        'account.account.template', string='Tax Received Account',
-        ondelete='restrict',
-        help='Account use when creating entry for tax cash basis')
-
-    def _get_tax_vals(self, company):
-        """ This method add in dictionnary of all the values for the tax that
-        will be created if will be assigned the cash basis account.
-        """
-        self.ensure_one()
-        res = super(AccountTaxTemplate, self)._get_tax_vals(company)
-        res.update({
-            'use_cash_basis': self.use_cash_basis,
-        })
-        return res
-
-    @api.multi
-    def _generate_tax(self, company):
-        """ This method update the return that generate taxes from templates.
-            :param company: the company for which the taxes should be created
-                from templates in self
-            :returns: {
-                'tax_template_to_tax': mapping between tax template and the
-                    newly generated taxes corresponding,
-                'account_dict': dictionary containing a to-do list with all
-                    the accounts to assign on new taxes
-            }
-        """
-        res = super(AccountTaxTemplate, self)._generate_tax(company)
-        for tax in self:
-            res.get('account_dict', {}).get(tax.id, {}).update({
-                'cash_basis_account': tax.cash_basis_account.id})
-        return res
+from openerp import models, api, _
 
 
 class AccountChartTemplate(models.Model):
@@ -54,38 +11,70 @@ class AccountChartTemplate(models.Model):
     def _load_template(
             self, company, code_digits=None, transfer_account_id=None,
             account_ref=None, taxes_ref=None):
-        """ Generate all the objects from the templates
-            :param company: company the wizard is running for
-            :param code_digits: number of digits the accounts code should have
-                in the COA
-            :param transfer_account_id: reference to the account template
-                that will be used as intermediary account for transfers between
-                2 liquidity accounts
-            :param acc_ref: Mapping between ids of account templates and real
-                accounts created from them
-            :param taxes_ref: Mapping between ids of tax templates and real
-                taxes created from them
-            :returns: tuple with a dictionary containing
-                * the mapping between the account template ids and the ids of
-                    the real accounts that have been generated
-                    from them, as first item,
-                * a similar dictionary for mapping the tax templates and taxes,
-                    as second item,
-            :rtype: tuple(dict, dict, dict)
-            inherited to write the cash_basis_account in the created taxes
+        """
+        Set the 'use_cash_basis' and 'cash_basis_account' fields on account.account. This hack is needed due to the fact
+        that the tax template does not have the fields 'use_cash_basis' and 'cash_basis_account'.
+
+        This hunk should be removed in master, as the account_tax_cash_basis module has been merged already in account
+        module
         """
         self.ensure_one()
         accounts, taxes = super(AccountChartTemplate, self)._load_template(
             company, code_digits=code_digits,
             transfer_account_id=transfer_account_id, account_ref=account_ref,
             taxes_ref=taxes_ref)
-        if account_ref is None:
-            account_ref = {}
+        if not self == self.env.ref('l10n_mx.mx_coa'):
+            return accounts, taxes
         account_tax_obj = self.env['account.tax']
+        account_obj = self.env['account.account']
+        taxes_acc = {
+            'ITAX_010-IN': account_obj.search([('code', '=', '208.01.01')]),
+            'ITAX_160-IN': account_obj.search([('code', '=', '208.01.01')]),
+            'ITAXR_04-OUT': account_obj.search([('code', '=', '216.13.01')]),
+            'ITAXR_10-OUT': account_obj.search([('code', '=', '216.13.01')]),
+            'ITAX_1067-OUT': account_obj.search([('code', '=', '216.13.01')]),
+            'ITAX_167-OUT': account_obj.search([('code', '=', '216.13.01')]),
+            'ITAX_010-OUT': account_obj.search([('code', '=', '208.01.01')]),
+            'ITAX_160-OUT': account_obj.search([('code', '=', '208.01.01')])}
 
         for tax in self.tax_template_ids:
+            if tax.description not in taxes_acc:
+                continue
             account_tax_obj.browse(taxes.get(tax.id)).write({
-                'cash_basis_account': account_ref.get(
-                    tax.cash_basis_account.id, False),
+                'use_cash_basis': True,
+                'cash_basis_account': taxes_acc.get(tax.description).id,
             })
         return accounts, taxes
+
+    @api.model
+    def generate_journals(self, acc_template_ref, company, journals_dict=None):
+        """Set the tax_cash_basis_journal_id on the company"""
+        res = super(AccountChartTemplate, self).generate_journals(
+            acc_template_ref, company, journals_dict=journals_dict)
+        if not self == self.env.ref('l10n_mx.mx_coa'):
+            return res
+        journal_basis = self.env['account.journal'].search([
+            ('type', '=', 'general'),
+            ('code', '=', 'CBMX')], limit=1)
+        company.write({'tax_cash_basis_journal_id': journal_basis.id})
+        return res
+
+    @api.multi
+    def _prepare_all_journals(self, acc_template_ref, company, journals_dict=None):
+        """Create the tax_cash_basis_journal_id"""
+        res = super(AccountChartTemplate, self)._prepare_all_journals(
+            acc_template_ref, company, journals_dict=journals_dict)
+        if not self == self.env.ref('l10n_mx.mx_coa'):
+            return res
+        res.append({
+            'type': 'general',
+            'name': _('Effectively Paid'),
+            'code': 'CBMX',
+            'company_id': company.id,
+            'default_credit_account_id': acc_template_ref.get(
+                self.income_currency_exchange_account_id.id),
+            'default_debit_account_id': acc_template_ref.get(
+                self.expense_currency_exchange_account_id.id),
+            'show_on_dashboard': True,
+        })
+        return res