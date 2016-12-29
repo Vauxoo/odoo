39aa37cb8e93d2cbcd8ea50c00987276d54cf206
Jose Suniagal [Vauxoo] <josemiguel@vauxoo.com>
2016-12-29 17:57:41 +0000

    [FIX] base_address_*: fixed city_id visibility on partner form view

--- addons/base_address_city/models/res_partner.py
+++ addons/base_address_city/models/res_partner.py
@@ -5,26 +5,30 @@ from lxml import etree
 
 from odoo import api, models, fields
 
+
 class FormatAddressMixin(models.AbstractModel):
     _inherit = "format.address.mixin"
 
     @api.model
-    def fields_view_get_address(self, arch):
-        arch = super(FormatAddressMixin, self).fields_view_get_address(arch)
-        #render the partner address accordingly to address_view_id
+    def _fields_view_get_address(self, arch):
+        arch = super(FormatAddressMixin, self)._fields_view_get_address(arch)
+        # render the partner address accordingly to address_view_id
         doc = etree.fromstring(arch)
         for city_node in doc.xpath("//field[@name='city']"):
-            replacement_xml = """
-            <div>
-            <field name="country_enforce_cities" invisible="1"/>
-            <div attrs="{'invisible': [('country_enforce_cities', '=', False)]}">
-                <field name='city' attrs="{'invisible': ['|', ('city_id', '!=', False), ('city', '=', False)]}"/>
-                <field name='city_id'/>
-            </div>
-            </div>
-            """
-            city_id_node = etree.fromstring(replacement_xml)
-            city_node.getparent().replace(city_node, city_id_node)
+            city_id_node = etree.fromstring(
+                '<field name="city_id" class="o_address_city"'
+                ' attrs="{\'invisible\':'
+                ' [(\'country_enforce_cities\', \'=\', False)]}"/>')
+            new_city_node = etree.fromstring(
+                '<field name="city" class="o_address_city"'
+                ' attrs="{\'invisible\':'
+                ' [(\'country_enforce_cities\', \'=\', True)]}"/>')
+            enforce_cities_node = etree.fromstring(
+                '<field name="country_enforce_cities" invisible="1"/>')
+            parent = city_node.getparent()
+            parent.replace(city_node, enforce_cities_node)
+            parent.insert(parent.index(enforce_cities_node)+1, new_city_node)
+            parent.insert(parent.index(new_city_node)+1, city_id_node)
 
         arch = etree.tostring(doc)
         return arch
@@ -33,7 +37,8 @@ class FormatAddressMixin(models.AbstractModel):
 class Partner(models.Model):
     _inherit = 'res.partner'
 
-    country_enforce_cities = fields.Boolean(related='country_id.enforce_cities')
+    country_enforce_cities = fields.Boolean(
+        related='country_id.enforce_cities')
     city_id = fields.Many2one('res.city', string='Company')
 
     @api.onchange('city_id')
