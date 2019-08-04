ba4b547bb641bcf4b24628e82375938b7ad67736
Arturo Flores <arturo@vauxoo.com>
2019-08-04 07:36:39 +0000

    [IMP] account_invoice_refund: improve how we get the accounts from product
    The previous approach was to add this function in with a wizard, but if
    we create a credit note without the wizard we are left outside of this
    feature. So we are removing the wizard and adding this validation at the
    level of line.

--- addons/account_invoice_refund/models/account.py
+++ addons/account_invoice_refund/models/account.py
@@ -4,13 +4,12 @@ from odoo import models
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    def get_invoice_line_account(self, type, product, fpos, company):
-        if type not in ('out_refund', 'in_refund'):
-            return super(AccountMoveLine, self).get_invoice_line_account(
-                type, product, fpos, company)
-        accounts = product.product_tmpl_id.get_product_accounts(fpos)
+    def _get_computed_account(self):
+        if self.move_id.type not in ('out_refund', 'in_refund'):
+            return super(AccountMoveLine, self)._get_computed_account()
+        accounts = self.product_id.product_tmpl_id.get_product_accounts()
         account_map = {
             'out_refund': 'income_refund',
             'in_refund': 'expense_refund',
         }
-        return accounts[account_map[type]]
+        return accounts[account_map[self.move_id.type]]
