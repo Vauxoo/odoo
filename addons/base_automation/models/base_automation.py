4d9f8e0a35567ff9ff5d92166db9d29d40aeb17f
Mitchelhdz <sebastian.hernandez@benandfrank.com>
2020-10-19 18:33:44 -0500

    [IMP] base_automation: Return early if there are not base.automation to process
    This is a backport from odoo/master to Vauxoo/12.0
    
    Installing base_automation module the methods: create, write, unlink and compute_field
    are patched.
    So, they will be used  for all models.
    It is important to save resources as possible.
    
    The patched methods in base.automation read the original data
    before to change so run all base.automation records.
    
    But What about if there are not base.automation records?
    So, we can save an extra read for all models
    
    The same to pre-filter and post-filter
    It adds a return early in order to skip this extra task when it will be useless.
    
    Co-authored-by: Moises Lopez <moylop260@vauxoo.com>
    
    Original commit: https://github.com/odoo/odoo/pull/52134/commits/fd59c74255462cc9ca2165213edf4ab48dc25dd2

--- addons/base_automation/models/base_automation.py
+++ addons/base_automation/models/base_automation.py
@@ -224,6 +224,8 @@ class BaseAutomation(models.Model):
             def create(self, vals_list, **kw):
                 # retrieve the action rules to possibly execute
                 actions = self.env['base.automation']._get_actions(self, ['on_create', 'on_create_or_write'])
+                if not actions:
+                    return create.origin(self, vals_list, **kw)
                 # call original method
                 records = create.origin(self.with_env(actions.env), vals_list, **kw)
                 # check postconditions, and execute actions on the records that satisfy them
@@ -243,6 +245,8 @@ class BaseAutomation(models.Model):
             def _write(self, vals, **kw):
                 # retrieve the action rules to possibly execute
                 actions = self.env['base.automation']._get_actions(self, ['on_write', 'on_create_or_write'])
+                if not (actions and self):
+                    return _write.origin(self, vals, **kw)
                 records = self.with_env(actions.env)
                 # check preconditions on records
                 pre = {action: action._filter_pre(records) for action in actions}
